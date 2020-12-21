b3519f34ed217a32e73e85c7a330d66e63c4d582
Daniel Lewandowski <dlewandowski38@yahoo.com>
2020-12-21 15:54:11 -0500

    language nav fixes, other small fixes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // @codekit-prepend "js/cognizant-framework-2020.js"
 
 //@codekit-append "lite-youtube-embed"
+
+AOS.init({
+    // duration: 750,
+    // mirror: false,
+    // startEvent: "load",
+    offset: "300"
+  });
